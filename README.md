534c94ec082793baeebc79c9d0a52b2853d8873c
marka2g <mark.sadegi@gmail.com>
2018-07-28 16:12:56 -0700

    add mix

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# emotion-of-state
-A map reduce exercise written in Elixir that parses the emotional state of The POTUS
+# Emotion of State
+A map reduce exercise written in Elixir that parses the emotional state of The POTUS via twitter.
---
+++ lib/emotion_of_state.ex
@@ -0,0 +1,18 @@
+defmodule EmotionOfState do
+  @moduledoc """
+  Documentation for EmotionOfState.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EmotionOfState.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
