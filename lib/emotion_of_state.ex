24b3a17879841baafc4c2f439de2e3362927cf1a
marka2g <mark.sadegi@gmail.com>
2018-07-28 22:26:55 -0700

    finish up parent process, input_reader, mapper, partition, reducer, output_writer and test file with notes

--- lib/emotion_of_state.ex
+++ lib/emotion_of_state.ex
@@ -6,6 +6,7 @@ defmodule EmotionOfState do
     args |> parse_args |> pipeline
   end
 
+  # for now, we'll just use the command line and pass in a arg to the module like `--file=potus_right_now.txt`
   defp parse_args(args) do
     {options, _, _} =
       OptionParser.parse(
@@ -19,9 +20,12 @@ defmodule EmotionOfState do
   defp pipeline([]) do
   end
 
+  # launch partition process, but only store process id in variable
+  # use elem to give us the second element of the tuple returned by starting our process because we don’t need the atom, :ok, first element.
+  # then the Partition process id and the file name are passed to an Input Reader that we will write in the next step
   defp pipeline(options) do
-    partition = elem(Partition.start_link(), 1)
-    InputReader.reader("#{options[:file]}", partition)
+    partition_process_id = elem(Partition.start_link(), 1)
+    InputReader.reader("#{options[:file]}", partition_process_id)
     forever()
   end
 
